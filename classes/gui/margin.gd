52ff7155aa9b753b83136a3f6189450f0e343efd
GTcreyon <67928545+GTcreyon@users.noreply.github.com>
2022-10-13 17:31:48 +0100

    Safe area corrections

--- classes/gui/margin.gd
+++ classes/gui/margin.gd
@@ -1,2 +1,12 @@
 extends Control
 
+func _process(_delta):
+	_update_safe_area()
+
+func _update_safe_area():
+	var safe_area: Rect2 = OS.get_window_safe_area()
+	var full_area = OS.window_size
+	margin_left = safe_area.position.x
+	margin_top = safe_area.position.y
+	margin_right = full_area.x - safe_area.size.x - safe_area.position.x
+	margin_right = full_area.y - safe_area.size.y - safe_area.position.y
